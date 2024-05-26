e2b3e2ac8361876d4ac0c1b22657a82b4faeb024
Shadeed <134431222+Sadeed000@users.noreply.github.com>
2024-05-26 19:32:27 +0530

    Update uploadImage.js

--- src/helpers/uploadImage.js
+++ src/helpers/uploadImage.js
@@ -1,16 +1,16 @@
-const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
+const url = `https://api.cloudinary.com/v1_1/dnsulnx1r/image/upload`
 
 const uploadImage  = async(image) => {
     const formData = new FormData()
-    formData.append('file',image)
+    formData.append("file",image)
     formData.append("upload_preset","mern_product")
+    formData.append("cloud_name",'dnsulnx1r')
     
-//    console.log(image)
+
     const dataResponse = await fetch(url,{
         method : "post",
         body : formData
     })
-    // console.log(dataResponse)
 
     return dataResponse.json()
 
